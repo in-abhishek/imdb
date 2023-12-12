96b469c7d0b9602700f00c6f11b35e0422f37148
Abhishek-1607 <abhishek16singh07@gmail.com>
2023-12-12 12:53:27 +0530

    All things have been done

--- README.md
+++ README.md
@@ -1,70 +1,31 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+ - What i explored
+     1. Project Setup 
+	 		Movies Listing
+			Search Movies
+			Filter Movies
+			Pagination
+	 2. How to fire API
+	 		https://developers.themoviedb.org/3/search/search-movies.  (documentation)
+	 3. Filter based on 
+	 		1. Search Movies 
+				Keyword must be in the input field
+				Keyword without case sensitive
+				Keyword minimum 3 characters 
+				Show data as per search result with pagination
+			2. Filter Movies 
+				Year must be in dropdown select
+				Year dropdown should be dynamic
+				Show data as per filter result with pagination
+			3. Pagination
+				Display pagination as per search result.
+				The pagination displayed must contain previous and next clickable links.
+				When the previous button is clicked it must redirect to the previous page
+	 4. Hooks Used to perform this task
+		 1. useEffect()
+		 2. useState()
+	  5. Key Points for learning
+	  	  1. for input field i used dedouncing method
+		  2. state management
+          3. array dependencies in useEffect 
+ - 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,299 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useRef, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import './App.css';
+import _ from 'lodash';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const App = () => {
+  const [data, setData] = useState("");
+  const [movieName, setMovieName] = useState();
+  const [loading, setLoading] = useState(true);
+  const [movieLength, setMovieLength] = useState(0);
+  const [error, setError] = useState({ show: "false", msg: "" });
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState();
+  const itemsPerPage = 18;
+  const [seledDate, setselectDate] = useState({
+    selectStartDate: '',
+    selectEndDate: '',
+  });
+  const [dateFiltersApplied, setDateFiltersApplied] = useState(false);
+  const ref = useRef(null);
+
+  // Fetch All MOvie Data
+  const fetchMovieData = async (page) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0612b4bc9527233a97fdbbce12a373de&page=${page}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const getMovieData = await response.json();
+      setLoading(false);
+      setData(getMovieData);
+      setTotalPage(getMovieData.total_pages);
+    } catch (error) {
+      setError({
+        show: "true",
+        msg: error,
+      })
+    }
+  };
+
+  // Debounce the search function
+
+  const toInputLowercase = (e) => {
+    const lowercasedValue = e.target.value.toLowerCase();
+    setMovieName(lowercasedValue);
+    debouncedSearch(lowercasedValue, movieLength, page);
+  };
+  // Search the query data from Api
+  const debouncedSearch = _.debounce(async (movieName, movieLength, page) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=d4fbc0cd7f3b6b7ea3c3b8e5c74b8f46&language=en-US&query=${movieName}&page=${page}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const getMovieData = await response.json();
+      setLoading(false);
+      setData(getMovieData);
+      setTotalPage(getMovieData.total_pages);
+    } catch (error) {
+      setError({
+        show: "true",
+        msg: error,
+      });
+    }
+  }, 500);
+
+  // useEffect(() => {
+  //   if (movieLength >= 3 || (movieName?.trim() ?? '') === '') {
+  //     debouncedSearch.cancel();
+  //     if ((movieName?.trim() ?? '') === '') {
+  //       fetchMovieData(page);
+  //       return;
+  //     }
+
+  //     debouncedSearch(movieName, movieLength, page);
+  //   }
+  // }, [movieName, movieLength, page, totalPage]);
+
+  const handleChange = (e) => {
+    const inputText = e.target.value.toLowerCase();
+    setMovieName(inputText);
+
+    let lengthMovie = inputText.length;
+
+    if (lengthMovie <= 3) {
+      setMovieLength(lengthMovie);
+    } else {
+      setMovieLength(0);
+    }
+    if (lengthMovie > 3) {
+      debouncedSearch(inputText, movieLength, page);
+    }
+  };
+
+  const pageCount = Math.ceil(totalPage / itemsPerPage);
+  // pagination click
+  const handlePageClick = (event) => {
+    if (data && data.total_pages) {
+      const newPage = event.selected + 1;
+      setPage(newPage);
+      ref.current?.scrollIntoView({ behavior: 'smooth' });
+
+      if (movieName) {
+        debouncedSearch(movieName, movieLength, newPage);
+      } else if (seledDate.selectStartDate !== '' && seledDate.selectEndDate !== '') {
+        fetchMovieDataViaDate(seledDate, newPage);
+      } else {
+        fetchMovieData(newPage);
+
+      }
+    }
+  };
+  const handleSelectChange = (e) => {
+    setMovieName(null)
+    const selectedIndex = e.target.value;
+    const getName = e.target.name;
+    if (getName === 'selectStartDate') {
+      setselectDate(prevState => ({ ...prevState, [getName]: selectedIndex, }));
+    }
+    else {
+      setselectDate(prevState => ({ ...prevState, [getName]: selectedIndex, }));
+    }
+  }
+  // Fetch All via Dates
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (seledDate.selectStartDate !== '' && seledDate.selectEndDate !== '') {
+          await fetchMovieDataViaDate(seledDate, page);
+          setDateFiltersApplied(true);
+        } else if (!dateFiltersApplied) {
+          await fetchMovieData(page);
+        }
+      } catch (error) {
+        setError({
+          show: true,
+          msg: error.message,
+        });
+      }
+    };
+
+    fetchData();
+  }, [seledDate.selectStartDate, seledDate.selectEndDate, page, dateFiltersApplied]);
+
+  const fetchMovieDataViaDate = async (seledDate, page) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0612b4bc9527233a97fdbbce12a373de&primary_release_date.gte=${seledDate.selectStartDate}&primary_release_date.lte=${seledDate.selectEndDate}&page=${page}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const getMovieData = await response.json();
+      if (getMovieData.total_pages === 0 && getMovieData.total_results === 0) {
+        setLoading(false);
+        setselectDate(({ selectStartDate: '', selectEndDate: '' }));
+      }
+      setLoading(false);
+      setData(getMovieData);
+      setTotalPage(getMovieData.total_pages);
+    } catch (error) {
+      setError({
+        show: "true",
+        msg: error,
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={ref}>
+      <div className="searchMovieInput">
+        <input type="text" placeholder='Enter movie name' className='searchMovie' value={movieName} onChange={handleChange} onInput={toInputLowercase} />
+      </div>
+      {loading ?
+        <div className="spinner">
+          <div className="loading-spinner"></div>
+        </div> : ''
+      }
+      {
+        data.results ? (
+          <>
+            <div className="dropdown-container">
+              <label htmlFor="">Start Date</label>
+              <select
+                name="selectStartDate"
+                id="selectStartDate"
+                onChange={handleSelectChange}
+                value={seledDate.selectStartDate}
+              >
+                <option value="" disabled>Select Start date</option>
+                {data.results.map((singleDropdown) => (
+                  <option key={singleDropdown.id} value={singleDropdown.release_date}>
+                    {singleDropdown.release_date}
+                  </option>
+                ))}
+              </select>
+
+              <div className="dropdown-container">
+                <label htmlFor="">End Date</label>
+                <select
+                  name="selectEndDate"
+                  id="selectEndDate"
+                  onChange={handleSelectChange}
+                  value={seledDate.selectEndDate}
+                >
+                  <option value="" disabled>Select End date</option>
+                  {data.results.map((singleDropdown) => (
+                    <option key={singleDropdown.id} value={singleDropdown.release_date}>
+                      {singleDropdown.release_date}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className='movieData'>
+              {
+                data.results && data.results.length > 0 ? (
+                  data.results.map((singleMovie, key) => {
+                    return (
+                      <div className='singleMovie' key={key}>
+                        <div className="row">
+                          <div className="col-4">
+                            {
+                              singleMovie.poster_path ?
+                                <img src={`https://image.tmdb.org/t/p/original${singleMovie.poster_path}`} alt="" /> :
+                                <img src={`https://placehold.co/80x80/000000/FFF`} alt="" />
+                            }
+                          </div>
+                          <div className="col-8">
+                            <div className="movieSerial">
+                              {
+                                singleMovie.title ?
+                                  <h2>{singleMovie.title} </h2>
+                                  : ''
+                              }
+                              {
+                                singleMovie.name ?
+                                  <h2>{singleMovie.name} </h2>
+                                  : ''
+                              }
+                              {singleMovie.overview ?
+                                <p>{singleMovie.overview}... </p>
+                                : ''
+                              }
+                            </div>
+                            <div className="movieSerialData">
+                              {
+                                singleMovie.release_date ?
+                                  <div className="serialSingleData">
+                                    <span>{singleMovie.release_date}</span>
+                                    <h3>Release Data</h3>
+                                  </div> : ''
+
+                              }
+
+                              {
+                                singleMovie.popularity ?
+                                  <div className="serialSingleData">
+                                    <span>{singleMovie.popularity}</span>
+                                    <h3>Popularity</h3>
+                                  </div> : ''
+                              }
+
+                              {
+                                singleMovie.vote_count ?
+                                  <div className="serialSingleData">
+                                    <span>{singleMovie.vote_count}</span>
+                                    <h3>Vote Count</h3>
+                                  </div> : ''
+                              }
+
+                            </div>
+                          </div>
+                        </div>
+                        <div className="popularity">
+                              {
+                                singleMovie.popularity ?
+                                <>
+                                  <span>Popularity:</span> <p>{singleMovie.popularity}</p> </>: ''
+                              }
+                            </div>
+                      </div>
+                    )
+                  })
+                ) : <span>No Results Found</span>
+              }
+            </div>
+            <div className='pagination'>
+              <ReactPaginate breakLabel="..." nextLabel="next >" onPageChange={handlePageClick} pageRangeDisplayed={2} 
+              pageCount={pageCount} previousLabel="< previous" renderOnZeroPageCount={null}
+              />
+            </div>
+          </>
+        ) : ''
+      }
+
     </div>
   );
 }
